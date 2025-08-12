ecd241449f164b0a7eb6aca574e937ef505e4ace
Your Name <sanreaksmey01@gmail.com>
2025-08-12 10:52:55 +0700

    Class login

--- Login.java
+++ Login.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Login {
     public static void main(String[] args) {
         System.out.println("Login System");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter name: ");
+        String name = scanner.next();
+        System.out.print("Enter Id: ");
+        int id = scanner.nextInt();
+
+        System.out.println("Your name: " + name + "Your ID: " + id);
+
+        scanner.close();
     }
 }
